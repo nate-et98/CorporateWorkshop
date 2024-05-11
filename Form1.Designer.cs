ac1f8d19f8e5c182017faeea90d77e86219609c0
nate-et98 <nathanettel@outlook.com>
2024-05-11 14:22:23 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace WorkshopSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listCities = new System.Windows.Forms.ListBox();
+            this.listWorkshops = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRegCost = new System.Windows.Forms.Label();
+            this.lblLodCost = new System.Windows.Forms.Label();
+            this.lblTotCost = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listCities
+            // 
+            this.listCities.FormattingEnabled = true;
+            this.listCities.ItemHeight = 20;
+            this.listCities.Items.AddRange(new object[] {
+            "Austin",
+            "Chicago",
+            "Dallas",
+            "Orlando",
+            "Phoenix",
+            "Raleigh"});
+            this.listCities.Location = new System.Drawing.Point(452, 57);
+            this.listCities.Name = "listCities";
+            this.listCities.Size = new System.Drawing.Size(191, 164);
+            this.listCities.TabIndex = 0;
+            this.listCities.SelectedIndexChanged += new System.EventHandler(this.listCities_SelectedIndexChanged);
+            // 
+            // listWorkshops
+            // 
+            this.listWorkshops.FormattingEnabled = true;
+            this.listWorkshops.ItemHeight = 20;
+            this.listWorkshops.Items.AddRange(new object[] {
+            "Handling Stress",
+            "Time Management",
+            "Supervision Skills",
+            "Negotiation",
+            "How To Interview"});
+            this.listWorkshops.Location = new System.Drawing.Point(155, 57);
+            this.listWorkshops.Name = "listWorkshops";
+            this.listWorkshops.Size = new System.Drawing.Size(203, 164);
+            this.listWorkshops.TabIndex = 1;
+            this.listWorkshops.SelectedIndexChanged += new System.EventHandler(this.listWorkshops_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Registration Cost:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lodging Cost:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total Cost:";
+            // 
+            // lblRegCost
+            // 
+            this.lblRegCost.AutoSize = true;
+            this.lblRegCost.Location = new System.Drawing.Point(469, 236);
+            this.lblRegCost.Name = "lblRegCost";
+            this.lblRegCost.Size = new System.Drawing.Size(18, 20);
+            this.lblRegCost.TabIndex = 5;
+            this.lblRegCost.Text = "0";
+            // 
+            // lblLodCost
+            // 
+            this.lblLodCost.AutoSize = true;
+            this.lblLodCost.Location = new System.Drawing.Point(469, 265);
+            this.lblLodCost.Name = "lblLodCost";
+            this.lblLodCost.Size = new System.Drawing.Size(18, 20);
+            this.lblLodCost.TabIndex = 6;
+            this.lblLodCost.Text = "0";
+            this.lblLodCost.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblTotCost
+            // 
+            this.lblTotCost.AutoSize = true;
+            this.lblTotCost.Location = new System.Drawing.Point(469, 298);
+            this.lblTotCost.Name = "lblTotCost";
+            this.lblTotCost.Size = new System.Drawing.Size(18, 20);
+            this.lblTotCost.TabIndex = 7;
+            this.lblTotCost.Text = "0";
+            this.lblTotCost.Click += new System.EventHandler(this.lblTotCost_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(322, 346);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(120, 38);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblTotCost);
+            this.Controls.Add(this.lblLodCost);
+            this.Controls.Add(this.lblRegCost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listWorkshops);
+            this.Controls.Add(this.listCities);
+            this.Name = "Form1";
+            this.Text = "Workshop Selection Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listCities;
+        private System.Windows.Forms.ListBox listWorkshops;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRegCost;
+        private System.Windows.Forms.Label lblLodCost;
+        private System.Windows.Forms.Label lblTotCost;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkshopSelector
+{
+    public partial class Form1 : Form
+    {
+        public int Days = 0;
+        public int RegCost = 0;
+        public int LodgeRate = 0;
+        public int LodgeCost = 0;
+        public int TotCost = 0;
+
+        /*
+        LodgeCost = Days * LodgeRate
+        TotCost = RegCost + LodgeCost
+        */
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotCost_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        //workshops
+        private void listWorkshops_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listWorkshops.SelectedIndex != -1)
+            {
+                //Handling Stress
+                if (listWorkshops.SelectedItem.ToString() == "Handling Stress"){
+                    Days = 3;
+                    RegCost = 1000;
+                }
+                //Time Management
+                if (listWorkshops.SelectedItem.ToString() == "Time Management")
+                {
+                    Days = 3;
+                    RegCost = 800;
+                }
+                //Supervision Skills
+                if (listWorkshops.SelectedItem.ToString() == "Supervision Skills")
+                {
+                    Days = 3;
+                    RegCost = 1500;
+                }
+                //Negotiation
+                if (listWorkshops.SelectedItem.ToString() == "Negotiation")
+                {
+                    Days = 5;
+                    RegCost = 1300;
+                }
+                //How To Interview
+                if (listWorkshops.SelectedItem.ToString() == "How To Interview")
+                {
+                    Days = 1;
+                    RegCost = 500;
+                }
+            }
+        }
+
+        //cities
+        private void listCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listCities.SelectedIndex != -1)
+            {
+                //Austin
+                if (listCities.SelectedItem.ToString() == "Austin")
+                {
+                    LodgeRate = 150;
+                }
+                //Chicago
+                if (listCities.SelectedItem.ToString() == "Chicago")
+                {
+                    LodgeRate = 225;
+                }
+                //Dallas
+                if (listCities.SelectedItem.ToString() == "Dallas")
+                {
+                    LodgeRate = 175;
+                }
+                //Orlando
+                if (listCities.SelectedItem.ToString() == "Orlando")
+                {
+                    LodgeRate = 300;
+                }
+                //Phoenix
+                if (listCities.SelectedItem.ToString() == "Phoenix")
+                {
+                    LodgeRate = 175;
+                }
+                //Raleigh
+                if (listCities.SelectedItem.ToString() == "Raleigh")
+                {
+                    LodgeRate = 150;
+                }
+            }
+        }
+
+        //calculate button
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            LodgeCost = Days * LodgeRate;
+            TotCost = RegCost + LodgeCost;
+
+            lblRegCost.Text = "$" + RegCost;
+            lblLodCost.Text = "$" + LodgeRate + " per night x " + Days+  " days = "+  LodgeCost;
+            lblTotCost.Text = "$" + TotCost;
+
+        }
+
+    }
+}
